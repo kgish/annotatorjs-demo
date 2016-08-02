3bfd4c8b3f0860a68a66e41a99ad587fb8d25ed2
Kiffin Gish <kiffin.gish@planet.nl>
2016-08-02 17:53:33 +0200

    Mirage implemented for topics.

--- mirage/factories/topic.js
+++ mirage/factories/topic.js
@@ -1,13 +1,42 @@
-import { Factory, faker } from 'ember-cli-mirage';
+import { Factory } from 'ember-cli-mirage';
+
+
+var topics = [
+    {
+        title: "Omnis Modi Qui Rerum Veniam Ipsam Consequatur",
+        author: "Estella Turcotte",
+        text: "Dolore nisi rerum sequi numquam consequatur. Incidunt neque id sunt ullam voluptates consequatur et. Occaecati accusantium at aut cupiditate. Quia nam est sit eum recusandae ab repudiandae adipisci. Voluptatem non voluptate dicta qui aut molestias facilis dolorem ipsam."
+    },
+    {
+        title: "Architecto Voluptates Modi",
+        author: "Akeem D'Amore",
+        text: "Excepturi consequatur molestias facilis reprehenderit rerum eligendi minima. Architecto inventore quis est et inventore. Enim officia beatae aut."
+    },
+    {
+        title: "Rem Est Odit",
+        author: "Liam Schulist",
+        text: "Magnam ullam mollitia amet molestiae ut cum. Sit est impedit aut. Doloremque ratione cupiditate consequuntur. Iste itaque ipsam iusto hic animi. Dicta aut natus ex qui id natus porro esse. Quia aut accusamus autem non quia."
+    },
+    {
+        title: "A Pariatur Consequatur Praesentium Exercitationem Minima Eos Inventore",
+        author: "Maggie Bode",
+        text: "Ducimus praesentium reiciendis odit atque fugit voluptatum sed libero saepe. Incidunt aut et iure suscipit fugiat inventore animi fugiat. Dolorem amet sunt praesentium et enim asperiores esse. Fugit est omnis minus voluptatem consequatur sit ut. Doloremque recusandae vitae laborum. Aliquid expedita qui sed qui."
+    },
+    {
+        title: "Earum Consequuntur Illo Cupiditate",
+        author: "Marcel Watsica",
+        text: "Modi nulla ut aut. Qui id sit eos perferendis amet. Voluptates pariatur dolores accusantium. Saepe quia animi aut. Aperiam illo qui quis nulla. Sint voluptas exercitationem aliquid."
+    }
+];
 
 export default Factory.extend({
-    title() {
-        return faker.lorem.words(Math.floor(Math.random() * 6) + 3);
+    title(i) {
+        return topics[i].title;
     },
-    author() {
-        return faker.name.firstName() + ' ' + faker.name.lastName();
+    author(i) {
+        return topics[i].author;
     },
-    text() {
-        return faker.lorem.paragraph();
+    text(i) {
+        return topics[i].text;
     }
 });
